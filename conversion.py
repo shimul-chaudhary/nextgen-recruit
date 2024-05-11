e940913c0d04879f502b0d2d1f254dd25014bf5a
Prachi Sethi <rumnraisin@Prachis-MacBook-Air.local>
2024-05-11 15:22:50 -0700

    added langchain utils

--- conversion.py
+++ conversion.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 n = 150
 
-df = pd.read_excel('data job posts.xlsx', sheet_name='Sheet1', usecols=["jobpost", "Title", "Company", "JobDescription"],
+df = pd.read_excel('data job posts.xlsx', sheet_name='Sheet1', usecols=["jobpost", "Title", "Company", "JobDescription","Location"],
         header=0, nrows=n)
 df = df.dropna()
 jsonl = df.to_json(orient="records", indent=0, lines=True)
