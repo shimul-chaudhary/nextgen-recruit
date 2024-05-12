7afc398723a4fb3df31ebcf2b740fbcb593caec7
Prachi Sethi <rumnraisin@Prachis-MacBook-Air.local>
2024-05-11 22:14:39 -0700

    applied jobs list updated

--- UI/app.py
+++ UI/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import json
+from utils import data
 
 
 st.title("NextGen Recruit")
@@ -7,37 +7,35 @@ if st.button("Add Job Post"):
     st.switch_page("pages/ad_posting.py")
 
 
-def read_jsonl(filename):
-  data = []
-  with open(filename, 'r') as f:
-    for line in f:
-      data.append(json.loads(line))
-  return data
 
-
-data = read_jsonl("../job-description.jsonl")  
 search_term = st.text_input("Search Jobs by Title (optional)")
-applied_jobs = set()
 
-def apply_button_clicked(job_title, company_name, location, row_index,description):
+def apply_button_clicked(job_title, company_name, location, row_index,description,flag,status):
   st.session_state["selected_job"] = {
       "job_title": job_title,
       "company_name": company_name,
       "location": location,
-      "description": description,  # Access job description
+      "description": description, 
+      "row_index":row_index,
+      "flag":flag,
+      "status":status
   }
   st.switch_page("pages/apply.py")
 
-# Search within the loop
+
 for row_index, row_data in enumerate(data):
   job_title = row_data.get("Title")
   company_name = row_data.get("Company")
   location = row_data.get("Location")
   description = row_data.get("JobDescription")
+  flag = row_data.get("flag")
+  status = row_data.get("status")
 
-  if search_term and search_term.lower() in job_title.lower():  # Check for search term in job title
-    st.write(f"Job Title: {job_title}")
-    st.write(f"Company Name: {company_name}")
-    st.write(f"Location: {location}")
-    if st.button(f"Apply", key=f"apply_button_{row_index}"):
-      apply_button_clicked(job_title, company_name, location, row_index,description)
+  if search_term and search_term.lower() in job_title.lower():  
+    if flag==False:
+        st.write(f"Job Title: {job_title}")
+        st.write(f"Company Name: {company_name}")
+        st.write(f"Location: {location}")
+        if st.button(f"Apply", key=f"apply_button_{row_index}"):
+            apply_button_clicked(job_title, company_name, location, row_index,description,flag,status)
+
---
+++ UI/utils.py
@@ -0,0 +1,23 @@
+import json
+
+job_list=[]
+def modify_jsonl(data):
+  modified_data = []
+  for item in data:
+    item["flag"] = False
+    item["status"] = "Not Applied"
+    modified_data.append(item)
+  return modified_data
+
+def read_jsonl_and_modify(filename):
+  data = []
+  with open(filename, 'r') as f:
+    for line in f:
+      try:
+        data.append(json.loads(line))
+      except json.JSONDecodeError:
+        print(f"Error: Could not parse line: {line}")
+
+  return modify_jsonl(data)
+
+data = read_jsonl_and_modify("../job-description.jsonl")  
